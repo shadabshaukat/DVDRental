21618f7104edf577098fa19cd8cbf0912c3b1c5a
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2025-05-08 22:46:40 +1000

    Update DVDRental_Redshift_Schema.sql

--- DVDRental_Redshift_Schema.sql
+++ DVDRental_Redshift_Schema.sql
@@ -16,195 +16,191 @@ drop table CITY ;
 drop table COUNTRY ;
 drop table CUSTOMER ;
 
--- Customer table
-CREATE TABLE customer (
-    customer_id INT ,
-    store_id SMALLINT,
-    first_name VARCHAR(45),
-    last_name VARCHAR(45),
-    email VARCHAR(50),
-    address_id INT,
-    activebool BOOLEAN,
-    create_date DATE,
-    last_update TIMESTAMP,
-    PRIMARY KEY (customer_id)
+-- Create table: actor
+CREATE TABLE actor (
+    actor_id INT  PRIMARY KEY,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
 )
 DISTSTYLE KEY
-DISTKEY (store_id)
-SORTKEY (last_name, first_name);
+DISTKEY (actor_id)
+SORTKEY (actor_id);
 
--- Store table
-CREATE TABLE store (
-    store_id SMALLINT ,
-    manager_staff_id SMALLINT,
-    address_id INT,
-    last_update TIMESTAMP,
-    PRIMARY KEY (store_id)
+-- Create table: address
+CREATE TABLE address (
+    address_id INT  PRIMARY KEY,
+    address VARCHAR(50) NOT NULL,
+    address2 VARCHAR(50),
+    district VARCHAR(20) NOT NULL,
+    city_id INT NOT NULL,
+    postal_code VARCHAR(10),
+    phone VARCHAR(20) NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
+)
+DISTSTYLE KEY
+DISTKEY (address_id)
+SORTKEY (address_id);
+
+-- Create table: category
+CREATE TABLE category (
+    category_id INT  PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
 )
 DISTSTYLE ALL
-SORTKEY (store_id);
+SORTKEY (category_id);
 
--- Staff table
-CREATE TABLE staff (
-    staff_id SMALLINT ,
-    first_name VARCHAR(45),
-    last_name VARCHAR(45),
-    address_id INT,
-    email VARCHAR(50),
-    store_id SMALLINT,
-    active BOOLEAN,
-    username VARCHAR(200),
-    password VARCHAR(32),
-    last_update TIMESTAMP,
-    PRIMARY KEY (staff_id)
-)
-DISTSTYLE EVEN
-SORTKEY (last_name, first_name);
-
--- Address table
-CREATE TABLE address (
-    address_id INT ,
-    address VARCHAR(50),
-    address2 VARCHAR(50),
-    district VARCHAR(20),
-    city_id INT,
-    postal_code VARCHAR(10),
-    phone VARCHAR(20),
-    last_update TIMESTAMP,
-    PRIMARY KEY (address_id)
-)
-DISTSTYLE EVEN
-SORTKEY (city_id, district);
-
--- City table
+-- Create table: city
 CREATE TABLE city (
-    city_id INT ,
-    city VARCHAR(50),
-    country_id INT,
-    last_update TIMESTAMP,
-    PRIMARY KEY (city_id)
+    city_id INT  PRIMARY KEY,
+    city VARCHAR(50) NOT NULL,
+    country_id INT NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
 )
-DISTSTYLE EVEN
+DISTSTYLE KEY
+DISTKEY (city_id)
+SORTKEY (city_id);
+
+-- Create table: country
+CREATE TABLE country (
+    country_id INT  PRIMARY KEY,
+    country VARCHAR(200) NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
+)
+DISTSTYLE ALL
 SORTKEY (country_id);
 
--- Country table
-CREATE TABLE country (
-    country_id INT ,
-    country VARCHAR(300),
+-- Create table: customer
+CREATE TABLE customer (
+    customer_id INT  PRIMARY KEY,
+    store_id INT NOT NULL,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    email VARCHAR(50),
+    address_id INT NOT NULL,
+    active INT DEFAULT 1 NOT NULL,
+    create_date DATE,
     last_update TIMESTAMP,
-    PRIMARY KEY (country_id)
-)
-DISTSTYLE ALL
-SORTKEY (country);
-
--- Film table
-CREATE TABLE film (
-    film_id INT ,
-    title VARCHAR(255),
-    description VARCHAR(1000),
-    release_year INT,
-    language_id INT,
-    rental_duration SMALLINT,
-    rental_rate DECIMAL(4,2),
-    length SMALLINT,
-    replacement_cost DECIMAL(5,2),
-    rating CHAR(5),
-    last_update TIMESTAMP,
-    PRIMARY KEY (film_id)
-)
-DISTSTYLE KEY
-DISTKEY (language_id)
-SORTKEY (title);
-
--- Language table
-CREATE TABLE language (
-    language_id INT,
-    name CHAR(20),
-    last_update TIMESTAMP,
-    PRIMARY KEY (language_id)
-)
-DISTSTYLE ALL
-SORTKEY (name);
-
--- Inventory table
-CREATE TABLE inventory (
-    inventory_id INT,
-    film_id INT,
-    store_id SMALLINT,
-    last_update TIMESTAMP,
-    PRIMARY KEY (inventory_id)
-)
-DISTSTYLE KEY
-DISTKEY (store_id)
-SORTKEY (film_id);
-
--- Rental table
-CREATE TABLE rental (
-    rental_id INT,
-    rental_date TIMESTAMP,
-    inventory_id INT,
-    customer_id INT,
-    return_date TIMESTAMP,
-    status varchar(50),
-    staff_id SMALLINT,
-    last_update TIMESTAMP,
-    PRIMARY KEY (rental_id)
+    activebool INT
 )
 DISTSTYLE KEY
 DISTKEY (customer_id)
-SORTKEY (rental_date);
+SORTKEY (customer_id);
 
--- Payment table
+-- Create table: film
+CREATE TABLE film (
+    film_id INT  PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(65535),
+    release_year INT,
+    language_id INT NOT NULL,
+    rental_duration INT NOT NULL DEFAULT 3,
+    rental_rate DECIMAL(4,2) DEFAULT 4.99 NOT NULL,
+    length INT,
+    replacement_cost DECIMAL(5,2) DEFAULT 19.99 NOT NULL,
+    rating VARCHAR(5) DEFAULT 'G',
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL,
+    special_features VARCHAR(500),
+    fulltext VARCHAR(65535)
+)
+DISTSTYLE KEY
+DISTKEY (film_id)
+SORTKEY (film_id);
+
+-- Create table: film_actor
+CREATE TABLE film_actor (
+    actor_id INT NOT NULL,
+    film_id INT NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL,
+    PRIMARY KEY (actor_id, film_id)
+)
+DISTSTYLE KEY
+DISTKEY (film_id)
+SORTKEY (film_id, actor_id);
+
+-- Create table: film_category
+CREATE TABLE film_category (
+    film_id INT NOT NULL,
+    category_id INT NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL,
+    PRIMARY KEY (film_id, category_id)
+)
+DISTSTYLE KEY
+DISTKEY (film_id)
+SORTKEY (film_id, category_id);
+
+-- Create table: inventory
+CREATE TABLE inventory (
+    inventory_id INT  PRIMARY KEY,
+    film_id INT NOT NULL,
+    store_id INT NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
+)
+DISTSTYLE KEY
+DISTKEY (inventory_id)
+SORTKEY (inventory_id);
+
+-- Create table: language
+CREATE TABLE language (
+    language_id INT  PRIMARY KEY,
+    name CHAR(20) NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
+)
+DISTSTYLE ALL
+SORTKEY (language_id);
+
+-- Create table: payment
 CREATE TABLE payment (
-    payment_id INT,
-    customer_id INT,
-    staff_id SMALLINT,
-    rental_id INT,
-    amount DECIMAL(5,2),
-    payment_date TIMESTAMP,
-    PRIMARY KEY (payment_id)
+    payment_id INT  PRIMARY KEY,
+    customer_id INT NOT NULL,
+    staff_id INT NOT NULL,
+    rental_id INT NOT NULL,
+    amount DECIMAL(5,2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT GETDATE() NOT NULL
 )
 DISTSTYLE KEY
 DISTKEY (customer_id)
 SORTKEY (payment_date);
 
--- Film_actor table
-CREATE TABLE film_actor (
-    actor_id INT,
-    film_id INT,
-    last_update TIMESTAMP,
-    PRIMARY KEY (actor_id, film_id)
+-- Create table: rental
+CREATE TABLE rental (
+    rental_id INT  PRIMARY KEY,
+    rental_date TIMESTAMP DEFAULT GETDATE() NOT NULL,
+    inventory_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    return_date TIMESTAMP,
+    status VARCHAR(50),
+    staff_id INT NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
 )
-DISTSTYLE EVEN
-SORTKEY (film_id);
+DISTSTYLE KEY
+DISTKEY (customer_id)
+SORTKEY (rental_date);
 
--- Film_category table
-CREATE TABLE film_category (
-    film_id INT,
-    category_id INT,
-    last_update TIMESTAMP,
-    PRIMARY KEY (film_id, category_id)
-)
-DISTSTYLE EVEN
-SORTKEY (category_id);
-
--- Actor table
-CREATE TABLE actor (
-    actor_id INT,
-    first_name VARCHAR(45),
-    last_name VARCHAR(45),
-    last_update TIMESTAMP,
-    PRIMARY KEY (actor_id)
-)
-DISTSTYLE EVEN
-SORTKEY (last_name, first_name);
-
--- Category table
-CREATE TABLE category (
-    category_id INT,
-    name VARCHAR(25),
-    last_update TIMESTAMP,
-    PRIMARY KEY (category_id)
+-- Create table: staff
+CREATE TABLE staff (
+    staff_id INT  PRIMARY KEY,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    address_id INT NOT NULL,
+    email VARCHAR(50),
+    store_id INT NOT NULL,
+    active SMALLINT DEFAULT 1 NOT NULL,
+    username VARCHAR(200) NOT NULL,
+    password VARCHAR(40),
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL,
+    picture VARCHAR(65535)
 )
 DISTSTYLE ALL
-SORTKEY (name);
+SORTKEY (staff_id);
+
+-- Create table: store
+CREATE TABLE store (
+    store_id INT  PRIMARY KEY,
+    manager_staff_id INT NOT NULL,
+    address_id INT NOT NULL,
+    last_update TIMESTAMP DEFAULT GETDATE() NOT NULL
+)
+DISTSTYLE ALL
+SORTKEY (store_id);
